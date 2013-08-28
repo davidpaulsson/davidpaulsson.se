5466d676222a0b2c882f405106f60fcc81b3635b
davidpaulsson <david@davidpaulsson.se>
2013-08-28 14:11:22 +0200

    Bower dependencies, new work section, sprites, etc

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-// @codekit-prepend "jquery.cycle2.js"
-// @codekit-prepend "jquery.cycle2.sparksHorz.min.js"
-// @codekit-prepend "jquery.cycle2.center.js"
-// @codekit-prepend "jquery.fitvids.js"
+// @codekit-prepend "../app/bower_components/jquery/jquery.js"
+// @codekit-prepend "../app/bower_components/fitvids/jquery.fitvids.js"
+// @codekit-prepend "../app/bower_components/OwlCarousel/owl-carousel/owl.carousel.js"
 
 jQuery(document).ready(function($) {
 
@@ -47,40 +46,59 @@ jQuery(document).ready(function($) {
 	});
 
 	//  Slideshow
-	var slideShow = $('.work ul');
-	slideShow.cycle({
-		autoHeight: 0,
-		fx: 'sparksHorz',
-		slides: '> li',
-		speed: 500,
-		timeout: 0,
-		centerHorz: true,
-		next: '.slide-next',
-		prev: '.slide-prev'
+	$('.owl-carousel').owlCarousel({
+		items: 3,
+		itemsDesktop: [1000,2], //5 items between 1000px and 901px
+		itemsDesktopSmall: false, // betweem 900px and 601px
+		itemsTablet: false, //2 items between 600 and 0;
+		itemsMobile: [479,1] // itemsMobile disabled - inherit from itemsTablet option
 	});
-	slideShow.on('cycle-update-view', function(e,o,sh,cs) {
-		var $this = $(this),
-		$currentSlide = $(cs);
 
-		$this.animate({
-			height: $currentSlide.height()
-		}, 300); // adjust height on slide transition
+	var owl = $('.owl-carousel').data('owlCarousel');
 
-		$(window).resize(function() {
-			$this.stop().animate({
-				height: $currentSlide.height()
-			}, 500);
-		});
+	$('.prev').click(function(event) {
+		owl.prev();
 	});
+
+	$('.next').click(function(event) {
+		owl.next();
+	});
+
+	//  Slideshow
+	// var slideShow = $('.work ul');
+	// slideShow.cycle({
+	// 	autoHeight: 0,
+	// 	fx: 'sparksHorz',
+	// 	slides: '> li',
+	// 	speed: 500,
+	// 	timeout: 0,
+	// 	centerHorz: true,
+	// 	next: '.slide-next',
+	// 	prev: '.slide-prev'
+	// });
+	// slideShow.on('cycle-update-view', function(e,o,sh,cs) {
+	// 	var $this = $(this),
+	// 	$currentSlide = $(cs);
+
+	// 	$this.animate({
+	// 		height: $currentSlide.height()
+	// 	}, 300); // adjust height on slide transition
+
+	// 	$(window).resize(function() {
+	// 		$this.stop().animate({
+	// 			height: $currentSlide.height()
+	// 		}, 500);
+	// 	});
+	// });
 
 	//  Fitvids
 	$('.single-post').fitVids();
 
 	//  Toggle clients
-	$('.clients h3').click(function() {
-		$(this).parent().toggleClass('active');
-		$('.logos').slideToggle();
-	});
+	// $('.clients h3').click(function() {
+	// 	$(this).parent().toggleClass('active');
+	// 	$('.logos').slideToggle();
+	// });
 
 	//  The most important feature on this site
 	var kkeys = [], konami = '38,38,40,40,37,39,37,39,66,65';
