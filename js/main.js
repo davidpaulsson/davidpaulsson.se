4be9e457747f8712109a1b5a4c9e86e999d2113b
davidpaulsson <david@davidpaulsson.se>
2013-08-29 11:59:57 +0200

    RSS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // @codekit-prepend "../app/bower_components/jquery/jquery.js"
 // @codekit-prepend "../app/bower_components/fitvids/jquery.fitvids.js"
 // @codekit-prepend "../app/bower_components/OwlCarousel/owl-carousel/owl.carousel.js"
+// @codekit-prepend "jquery.zrssfeed.js"
 
 jQuery(document).ready(function($) {
 
@@ -46,6 +47,7 @@ jQuery(document).ready(function($) {
 	});
 
 	//  Slideshow
+	var owl = $('.owl-carousel').data('owlCarousel');
 	$('.owl-carousel').owlCarousel({
 		items: 3,
 		itemsDesktop: [1000,2], //5 items between 1000px and 901px
@@ -53,52 +55,23 @@ jQuery(document).ready(function($) {
 		itemsTablet: false, //2 items between 600 and 0;
 		itemsMobile: [479,1] // itemsMobile disabled - inherit from itemsTablet option
 	});
-
-	var owl = $('.owl-carousel').data('owlCarousel');
-
 	$('.prev').click(function(event) {
 		owl.prev();
 	});
-
 	$('.next').click(function(event) {
 		owl.next();
 	});
 
-	//  Slideshow
-	// var slideShow = $('.work ul');
-	// slideShow.cycle({
-	// 	autoHeight: 0,
-	// 	fx: 'sparksHorz',
-	// 	slides: '> li',
-	// 	speed: 500,
-	// 	timeout: 0,
-	// 	centerHorz: true,
-	// 	next: '.slide-next',
-	// 	prev: '.slide-prev'
-	// });
-	// slideShow.on('cycle-update-view', function(e,o,sh,cs) {
-	// 	var $this = $(this),
-	// 	$currentSlide = $(cs);
-
-	// 	$this.animate({
-	// 		height: $currentSlide.height()
-	// 	}, 300); // adjust height on slide transition
-
-	// 	$(window).resize(function() {
-	// 		$this.stop().animate({
-	// 			height: $currentSlide.height()
-	// 		}, 500);
-	// 	});
-	// });
-
 	//  Fitvids
 	$('.single-post').fitVids();
 
-	//  Toggle clients
-	// $('.clients h3').click(function() {
-	// 	$(this).parent().toggleClass('active');
-	// 	$('.logos').slideToggle();
-	// });
+	//  Latest comments
+	$('.latest-comments-load').rssfeed('http://davidpaulsson.disqus.com/latest.rss', {
+		limit: 5,
+		date: false,
+		header: false,
+		showerror: false
+	});
 
 	//  The most important feature on this site
 	var kkeys = [], konami = '38,38,40,40,37,39,37,39,66,65';
