04e58ab550faa1e6583b4b326d4e025a02a8f95b
David Paulsson <david@davidpaulsson.se>
2020-04-05 22:18:33 +0200

    Feat: dark mode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,3 +10,9 @@ import './src/styles/reset.scss';
 import './src/styles/variables.scss';
 import './src/styles/global.scss';
 import './src/styles/fonts.scss';
+
+// detect and toggle dark mode
+const htmlTag = document.body.parentNode;
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  htmlTag.classList.add('__dark-mode');
+}
