d893f88c1372ef51cf2e2338731d80d27387c985
David Paulsson <david@davidpaulsson.se>
2020-04-05 21:48:58 +0200

    Feat: new site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,13 +56,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `davidpaulsson`,
+        short_name: `davidpaulsson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/davidpaulsson-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
