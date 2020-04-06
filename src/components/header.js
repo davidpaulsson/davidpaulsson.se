56620bdc7ba8c8173b592f7abe168a09fe5f218e
David Paulsson <david@davidpaulsson.se>
2020-04-06 10:28:17 +0200

    Refactor: extract toggle

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import { AnimatePresence, motion, useCycle } from 'framer-motion';
 import React from 'react';
+import Toggle from './toggle';
 import './header.scss';
 
 const Header = ({ headline, html }) => {
-  const [isOpen, toggleIsOpen] = useCycle(false, true);
+  const [isOpen, setIsOpen] = useCycle(false, true);
 
   return (
     <header className="header">
@@ -12,73 +13,17 @@ const Header = ({ headline, html }) => {
         dangerouslySetInnerHTML={{ __html: headline }}
       />
 
-      <motion.button
-        className="header__toggle"
-        onClick={toggleIsOpen}
-        animate={!isOpen ? 'open' : 'closed'}
-        variants={{
-          open: {
-            opacity: 1,
-            transition: {
-              staggerChildren: 0.1,
-              staggerDirection: -1,
-            },
-          },
-          closed: {
-            opacity: 0.8,
-            transition: {
-              staggerChildren: 0.1,
-            },
-          },
-        }}
-      >
-        <motion.svg viewBox="0 0 24 8" width="24" height="8" initial="closed">
-          <motion.circle cx="4" cy="4" r="1" />
-          <motion.circle
-            cx="12"
-            cy="4"
-            r="1"
-            variants={{
-              open: { cx: 12 },
-              closed: { cx: 4 },
-            }}
-          />
-          <motion.circle
-            cx="20"
-            cy="4"
-            r="1"
-            variants={{
-              open: { cx: 20 },
-              closed: { cx: 4 },
-            }}
-          />
-
-          <motion.line
-            x1="2"
-            y1="4"
-            x2="22"
-            y2="4"
-            style={{ strokeWidth: 4 }}
-            variants={{
-              open: { opacity: 1, x2: 2 },
-              closed: { opacity: 1, x2: 22 },
-            }}
-          />
-        </motion.svg>
-      </motion.button>
+      <Toggle {...{ isOpen, setIsOpen }} />
 
       <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            className="h2 header__content"
-            style={{ overflow: 'hidden' }}
-            initial={{ height: 0 }}
-            animate={{ height: 'auto' }}
-            exit={{ height: 0 }}
-            // transition={{ duration: 1 }}
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        )}
+        <motion.div
+          className="h2 header__content"
+          style={{ overflow: 'hidden' }}
+          initial={{ height: 0 }}
+          animate={{ height: isOpen ? 'auto' : 0 }}
+          exit={{ height: 0 }}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </AnimatePresence>
     </header>
   );
---
+++ src/components/toggle.js
@@ -0,0 +1,81 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+import './toggle.scss';
+
+const Toggle = ({ isOpen, setIsOpen }) => (
+  <motion.button
+    className="toggle"
+    onClick={setIsOpen}
+    animate={!isOpen ? 'open' : 'closed'}
+    variants={{
+      open: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.1,
+          staggerDirection: -1,
+        },
+      },
+      closed: {
+        opacity: 0.8,
+        transition: {
+          staggerChildren: 0.1,
+        },
+      },
+    }}
+  >
+    <div className="toggle__inner">
+      <motion.svg viewBox="0 0 28 12" width="28" height="12" initial="closed">
+        <motion.circle cx="6" cy="6" r="1" />
+        <motion.circle
+          cx="14"
+          cy="6"
+          r="1"
+          variants={{
+            open: {
+              cx: 14,
+            },
+            closed: {
+              cx: 6,
+            },
+          }}
+        />
+        <motion.circle
+          cx="22"
+          cy="6"
+          r="1"
+          variants={{
+            open: {
+              cx: 22,
+            },
+            closed: {
+              cx: 6,
+            },
+          }}
+        />
+
+        <motion.line
+          x1="6"
+          y1="6"
+          x2="22"
+          y2="6"
+          style={{
+            strokeWidth: 4,
+          }}
+          strokeLinecap="round"
+          variants={{
+            open: {
+              opacity: 1,
+              x2: 6,
+            },
+            closed: {
+              opacity: 1,
+              x2: 22,
+            },
+          }}
+        />
+      </motion.svg>
+    </div>
+  </motion.button>
+);
+
+export default Toggle;
