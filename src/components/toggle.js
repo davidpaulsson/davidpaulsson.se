730f4321ac1ad5d15ad365cdbdc75b9a854e5b95
David Paulsson <david@davidpaulsson.se>
2020-04-06 11:34:36 +0200

    Style: make dots wait for the user

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,89 +1,139 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './toggle.scss';
 
-const Toggle = ({ isOpen, setIsOpen }) => (
-  <motion.button
-    className="toggle"
-    onClick={setIsOpen}
-    animate={!isOpen ? 'open' : 'closed'}
-    variants={{
-      open: {
-        opacity: 1,
-        // transition: {
-        //   staggerChildren: 0.1,
-        //   staggerDirection: -1,
-        // },
-      },
-      closed: {
-        opacity: 0.6,
-        // transition: {
-        //   staggerChildren: 0.1,
-        // },
-      },
-    }}
-  >
-    <div className="toggle__inner">
-      <motion.svg viewBox="0 0 28 12" width="28" height="12" initial="closed">
-        <motion.circle
-          cx="6"
-          cy="6"
-          r="1"
-          // variants={{ open: { cy: 4 } }}
-        />
-        <motion.circle
-          cx="14"
-          cy="6"
-          r="1"
-          variants={{
-            open: {
-              cx: 14,
-              // cy: 4,
-            },
-            closed: {
-              cx: 6,
-            },
-          }}
-          transition={{ delay: isOpen ? 0 : 0.2 }}
-        />
-        <motion.circle
-          cx="22"
-          cy="6"
-          r="1"
-          variants={{
-            open: {
-              cx: 22,
-              // cy: 4,
-            },
-            closed: {
-              cx: 6,
-            },
-          }}
-          transition={{ delay: isOpen ? 0.2 : 0.4 }}
-        />
+const Toggle = ({ isOpen, setIsOpen }) => {
+  const [state, setState] = useState(isOpen ? 'open' : 'closed');
 
-        <motion.line
-          x1="6"
-          y1="6"
-          x2="22"
-          y2="6"
-          style={{
-            strokeWidth: 4,
-          }}
-          strokeLinecap="round"
-          variants={{
-            open: {
-              x2: 6,
-            },
-            closed: {
-              x2: 22,
-            },
-          }}
-          transition={{ delay: isOpen ? 0.4 : 0, type: 'tween' }}
-        />
-      </motion.svg>
-    </div>
-  </motion.button>
-);
+  useEffect(() => {
+    setState(isOpen ? 'open' : 'closed');
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (state === 'closed') {
+      setTimeout(() => {
+        setState('waiting');
+      }, 2000);
+    }
+  }, [state]);
+
+  console.log({ state });
+
+  return (
+    <motion.button
+      className="toggle"
+      onClick={setIsOpen}
+      animate={state}
+      variants={{
+        open: {
+          opacity: 0.6,
+        },
+        closed: {
+          opacity: 1,
+        },
+      }}
+    >
+      <div className="toggle__inner">
+        <motion.svg viewBox="0 0 28 12" width="28" height="12" initial="closed">
+          <motion.circle
+            cx="6"
+            cy="6"
+            r="1"
+            variants={{
+              open: { cy: 6, opacity: 1 },
+              closed: { cy: 6, opacity: 1 },
+              waiting: {
+                cy: [6, 5, 6],
+                opacity: [0.6, 1, 0.6],
+                transition: {
+                  duration: 0.4,
+                  loop: Infinity,
+                  repeatDelay: 2,
+                },
+              },
+            }}
+          />
+          <motion.circle
+            cx="14"
+            cy="6"
+            r="1"
+            variants={{
+              open: {
+                cx: 6,
+                cy: 6,
+                opacity: 1,
+              },
+              closed: {
+                cx: 14,
+                cy: 6,
+                opacity: 1,
+              },
+              waiting: {
+                cy: [6, 5, 6],
+                opacity: [0.6, 1, 0.6],
+                transition: {
+                  duration: 0.4,
+                  delay: 0.2,
+                  loop: Infinity,
+                  repeatDelay: 2,
+                },
+              },
+            }}
+            transition={{ delay: state === 'open' ? 0 : 0.2 }}
+          />
+          <motion.circle
+            cx="22"
+            cy="6"
+            r="1"
+            variants={{
+              open: {
+                cx: 6,
+                cy: 6,
+                opacity: 1,
+              },
+              closed: {
+                cx: 22,
+                cy: 6,
+                opacity: 1,
+              },
+              waiting: {
+                cy: [6, 5, 6],
+                opacity: [0.6, 1, 0.6],
+                transition: {
+                  duration: 0.4,
+                  delay: 0.4,
+                  loop: Infinity,
+                  repeatDelay: 2,
+                },
+              },
+            }}
+            transition={{ delay: state === 'open' ? 0.2 : 0.4 }}
+          />
+
+          <motion.line
+            x1="6"
+            y1="6"
+            x2="22"
+            y2="6"
+            style={{
+              opacity: state === 'waiting' ? 0 : 1,
+              strokeWidth: 4,
+            }}
+            strokeLinecap="round"
+            variants={{
+              open: {
+                x2: 22,
+              },
+              closed: {
+                x2: 6,
+              },
+            }}
+            transition={{ delay: state === 'open' ? 0.4 : 0, type: 'tween' }}
+          />
+        </motion.svg>
+      </div>
+    </motion.button>
+  );
+};
 
 export default Toggle;
