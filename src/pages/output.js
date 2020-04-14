f3d6261fdc68df8460156b26dd3856627787cdf0
David Paulsson <david@davidpaulsson.se>
2020-04-14 07:47:58 +0200

    Fix: bgc cover transition

--- src/pages/output.js
+++ src/pages/output.js
@@ -20,15 +20,7 @@ const IndexPage = () => {
         <Input />
       </motion.div>
       <motion.div
-        style={{
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          backgroundColor: 'var(--background)',
-          opacity: 0.75,
-        }}
+        className="cover"
         animate={{ opacity: 0.5 }}
         transition={{ ease: 'easeInOut', duration: 2 }}
         exit={{ opacity: 0 }}
